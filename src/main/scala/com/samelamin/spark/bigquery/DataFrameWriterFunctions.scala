d8af9189295c073b3ea9ec61c5eff466536e30ff
holamap <holamap@users.noreply.github.com>
2018-07-16 14:39:04 +0530

    forked package name changed
    forked package name changed

--- src/main/scala/com/samelamin/spark/bigquery/DataFrameWriterFunctions.scala
+++ src/main/scala/com/samelamin/spark/bigquery/DataFrameWriterFunctions.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.samelamin.spark.bigquery
+package com.samelaminfork.spark.bigquery
 
 import org.apache.spark.sql.{DataFrameWriter, Row}
 
