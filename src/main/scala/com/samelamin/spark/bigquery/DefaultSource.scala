25d6b21112b8004100c9fe59f21c5240c4679ebb
holamap <holamap@users.noreply.github.com>
2018-07-16 14:38:32 +0530

    forked package name changed
    forked package name changed

--- src/main/scala/com/samelamin/spark/bigquery/DefaultSource.scala
+++ src/main/scala/com/samelamin/spark/bigquery/DefaultSource.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.samelamin.spark.bigquery
+package com.samelaminfork.spark.bigquery
 
 import com.google.cloud.hadoop.io.bigquery.BigQueryStrings
 import com.samelamin.spark.bigquery.converters.SchemaConverters
